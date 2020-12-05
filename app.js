2730a3fcf2c2ad38bf8fb4a7064e1ae95d464b63
Sanket Manmode <manmodesanket@gmail.com>
2020-12-05 15:17:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+var textInput = document.querySelector("#txt-input");
+
+var textOutput = document.querySelector("#txt-output");
+
+var url = "https://api.funtranslations.com/translate/dothraki.json";
+
+var errorHandler = (err) => {
+    console.log("ERROR OCCURED");
+    alert("Error " + err);
+}
+
+btnTranslate.addEventListener("click", () => {
+    var url2 = url + "?text=" + textInput.value;
+    console.log(url2); 
+    fetch(url2).then((res) => res.json()).then((res) => {
+        console.log(res);
+        textOutput.value = res.contents.translated;
+    })
+    .catch(errorHandler)
+})
